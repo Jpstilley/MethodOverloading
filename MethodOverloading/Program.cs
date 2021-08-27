0e782353ead5462204423cccc967b24b2a80bd66
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-27 11:38:27 -0500

    added console output for all methods and method outputs

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,7 +6,11 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Add(14, 16));
+            Console.WriteLine(Add(0.5m, 2.25m));
             Console.WriteLine(Add(0, 1, false));
+            Console.WriteLine(Add(0, 1, true));
+            Console.WriteLine(Add(3, 1, true));
         }
 
         public static int Add(int x, int y)
