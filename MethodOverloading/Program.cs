102024dbaa45a90291b4a81c5d6f36c0f34f7be1
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-27 11:30:49 -0500

    Completed the exercise

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(0, 1, false));
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            string plural = (x + y > 1) ? "s" : "";
+            return (isTrue) ? $"{x + y} dollar{plural}"
+                            : $"We're not allowed to add your numbers";
+        }
+    }
+}
